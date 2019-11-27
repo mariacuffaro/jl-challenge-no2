b8d815241442a4dfb9f92806ee31aabdfdc8d55a
mariacuffaro <maria.cuffaro@johnlewis.co.uk>
2019-11-27 15:30:14 +0000

    add comments to describe process and new test

--- spec/fixPriceLabelSpec.js
+++ spec/fixPriceLabelSpec.js
@@ -1,7 +1,11 @@
 describe('fixPriceLabel', function() {
     it('outputs input if input is valid', function(){
-        let inputLabel = 'Was £11, now £6'
-        expect(fixPriceLabel(inputLabel)).toEqual(inputLabel);
-      });
-    
+      let inputLabel = 'Was £11, now £6'
+      expect(fixPriceLabel(inputLabel)).toEqual(inputLabel);
+    });
+    xit('corrects label if not valid', function(){
+      let inputLabel = 'Was £10, then £8, then £11, now £6'
+      let outputLabel = 'Was £11, now £6'
+      expect(fixPriceLabel(inputLabel)).toEqual(outputLabel);
+    });
 });
--- src/fixPriceLabel.js
+++ src/fixPriceLabel.js
@@ -1,3 +1,13 @@
 function fixPriceLabel(inputLabel) {
+    var prices = inputLabel.match(/\d+/g).map(Number);
+    console.log(prices);
+    // for each price
+    // move 1st price to new Array
+    // if price is less than last price add to new array 
+    // if price is bigger than last price, empty new array and add price to empty array
+    // for each price in new array
+    // Add 'Was £' string to 1st price
+    // Add ', then £' to price
+    // Add ', now £ to last price
     return inputLabel;
 };
