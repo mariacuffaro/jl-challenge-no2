0b836b1a33dd0a723349334ee8c93e547537e8d7
mariacuffaro <maria.cuffaro@johnlewis.co.uk>
2019-11-27 17:25:11 +0000

    iterate array to create output string

--- spec/fixPriceLabelSpec.js
+++ spec/fixPriceLabelSpec.js
@@ -1,11 +1,11 @@
-describe('fixPriceLabel', function() {
+describe('FixPriceLabel', function() {
     it('outputs input if input is valid', function(){
       let inputLabel = 'Was £11, now £6'
-      expect(fixPriceLabel(inputLabel)).toEqual(inputLabel);
+      expect(FixPriceLabel(inputLabel)).toEqual(inputLabel);
     });
-    xit('corrects label if not valid', function(){
+    it('corrects label if not valid', function(){
       let inputLabel = 'Was £10, then £8, then £11, now £6'
       let outputLabel = 'Was £11, now £6'
-      expect(fixPriceLabel(inputLabel)).toEqual(outputLabel);
+      expect(FixPriceLabel(inputLabel)).toEqual(outputLabel);
     });
 });
--- src/fixPriceLabel.js
+++ src/fixPriceLabel.js
@@ -1,13 +1,30 @@
-function fixPriceLabel(inputLabel) {
-    var prices = inputLabel.match(/\d+/g).map(Number);
-    console.log(prices);
+function FixPriceLabel(inputLabel) {
+    let prices = inputLabel.match(/\d+/g).map(Number);
+    let lastPrice = 0;
+
     // for each price
-    // move 1st price to new Array
-    // if price is less than last price add to new array 
-    // if price is bigger than last price, empty new array and add price to empty array
+    let validPrices = [];
+    prices.forEach(validatePrices);
+
+    function validatePrices(price, index) {
+        // move 1st price to new Array
+        if (index == 0) { lastPrice = price;  validPrices.push(price) };
+        // if price is less than last price add to new array 
+        if (lastPrice > price) { lastPrice = price;  validPrices.push(price) }
+        // if price is bigger than last price, empty new array and add price to empty array
+        if (lastPrice < price) { lastPrice = price;  validPrices = []; validPrices.push(price) }
+    };
+
+    outputLabel = '';
     // for each price in new array
-    // Add 'Was £' string to 1st price
-    // Add ', then £' to price
-    // Add ', now £ to last price
-    return inputLabel;
+    validPrices.forEach(createOutputString);
+    function createOutputString(price, index) {
+        // Add 'Was £' string to 1st price
+        if (index == 0) { outputLabel = `Was £${price}`}
+        // Add ', now £ to last price
+        if (index == validPrices.length - 1) { outputLabel = outputLabel + `, now £${price}`}
+        // Add ', then £' to price
+    };
+    
+    return outputLabel;
 };
